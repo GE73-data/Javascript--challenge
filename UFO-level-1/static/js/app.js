fbb8e6eac235f23a82b39c515fb964272054322e
GE73-data <gloria.evans73@yahoo.com>
2021-08-12 18:51:54 -0400

    complete assignment

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,54 +3,24 @@ var tableData = data;
 
 // table body
 var tbody = d3.select("tbody")
+function buildTable(incomingdata) {
+    tbody.html("")
+    // Sighting values for each column
+    incomingdata.forEach(function (ufoSighting) {
+        
+        //  Append table row for sighting object
+        var row = tbody.append("tr");
 
-// Sighting values for each column
-data.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
-
-//  Append table row for sighting object
-    var row= tbody.append("tr");
-
-//  Use entries to console.log each UFO Sighting
-    Object.entries(ufoSighting).forEach(function([key, value]) {
-        console.log(key, value);
-
-//  Append a cell to the row
-        var cell = row.append("td");
-        cell.text(value);
+        //  Use entries to console.log each UFO Sighting
+        Object.entries(ufoSighting).forEach(function ([key, value]) {
+            
+            //  Append a cell to the row
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+}
 
-// Select button
-var button = d3.select("#filter-btn");
-button.on("click", function() {
-    tbody.html("");
-
-//Prevent the page from refreshing
-    d3.event.preventDefault();
-
-//     Select the date 
-    var inputElement = d3.select("#datetime");
-
-//     Get the value property
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
-
-//    Filter Data with datetime = input
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-    console.log(filteredData);
-
-    filteredData.forEach(function(selections) {
-    console.log(selections);
-
-//   Append one table row for each UFO sighting
-    var row = tbody.append("tr");
-
-//  Object.entries(selections).forEach(function([key,value]) {
-        console.log(key,value);
-        var cell = row.append("td");cell.text(value);
-    });
-    })
 
 //Click function
 function handleClick() {
@@ -71,6 +41,7 @@ function handleClick() {
 d3.selectAll("#filter-btn").on("click", handleClick);
 
 //Build Table
-// buildTable(tableData);
+buildTable(tableData);
+
 
 
